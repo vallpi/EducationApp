a18e059ad140b483264c5369ef2e6a9f36cd5c7d
Sandvie <39433637+Sandvie@users.noreply.github.com>
2018-06-02 23:13:49 +0300

    v 1.0.7.2

--- EducationApp/SubjectInfo.xaml.cs
+++ EducationApp/SubjectInfo.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using App.Classes;
+using App.Classes.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,35 @@ namespace EducationApp
     /// </summary>
     public partial class SubjectInfo : Page
     {
+        IRepository _repo = Factory.Instance.GetRepository();
         public SubjectInfo()
         {
             InitializeComponent();
+            textBlock_Subject1.Text = _repo.GetSubject(1);
+            textBlock_Subject2.Text = _repo.GetSubject(2);
+            textBlock_Subject3.Text = _repo.GetSubject(3);
+            listBox_Themes.ItemsSource = _repo.ReturnSubjectTopics();
+        }
+
+        private void Hyperlink1_Click(object sender, RoutedEventArgs e)
+        {
+            _repo.SelectSubject(1);
+            listBox_Themes.ItemsSource = null;
+            listBox_Themes.ItemsSource = _repo.ReturnSubjectTopics();
+        }
+
+        private void Hyperlink2_Click(object sender, RoutedEventArgs e)
+        {
+            _repo.SelectSubject(2);
+            listBox_Themes.ItemsSource = null;
+            listBox_Themes.ItemsSource = _repo.ReturnSubjectTopics();
+        }
+
+        private void Hyperlink3_Click(object sender, RoutedEventArgs e)
+        {
+            _repo.SelectSubject(3);
+            listBox_Themes.ItemsSource = null;
+            listBox_Themes.ItemsSource = _repo.ReturnSubjectTopics();
         }
 
         private void ButtonStartTest_Click(object sender, RoutedEventArgs e)
